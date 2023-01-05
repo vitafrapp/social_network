2dde32be6975ba75e15bb1cbbac88664989fbff1
Li Shi <lshi97@gatech.edu>
2023-01-04 21:01:08 -0600

    minor bug fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,7 +31,9 @@ router.post(
       // See if user exists
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       // Get users gravatar
